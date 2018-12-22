b8837d96744d95700263e1a234ea59b61206cb4b
lmxyy <lmxyy1999@foxmail.com>
2018-12-22 21:29:34 +0800

    remove sw remove handler

--- assignment5/progAssign4/src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
+++ assignment5/progAssign4/src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
@@ -179,12 +179,7 @@ public class L3Routing implements IFloodlightModule, IOFSwitchListener,
                 Path path = entry.getValue();
                 OFMatch ofMatch = new OFMatch();
                 ofMatch.setDataLayerType(OFMatch.ETH_TYPE_IPV4);
-                assert dst.getIPv4Address() != null;
-                assert dst != null;
-                assert ofMatch != null;
-                System.err.println(dst.getIPv4Address());
-                OFMatch ofMatch1 = new OFMatch();
-                ofMatch1.setNetworkDestination(0);
+                if (dst.getIPv4Address() == null) continue;
                 ofMatch.setNetworkDestination(dst.getIPv4Address());
                 OFActionOutput ofActionOutput = new OFActionOutput(path.end().getValue());
                 OFInstructionApplyActions ofInstructionApplyActions = new OFInstructionApplyActions(new ArrayList<OFAction>(Collections.singletonList(ofActionOutput)));
@@ -200,12 +195,7 @@ public class L3Routing implements IFloodlightModule, IOFSwitchListener,
                 IOFSwitch iofSwitch = entry.getKey();
                 OFMatch ofMatch = new OFMatch();
                 ofMatch.setDataLayerType(OFMatch.ETH_TYPE_IPV4);
-                assert dst.getIPv4Address() != null;
-                assert dst != null;
-                assert ofMatch != null;
-                System.err.println(dst.getIPv4Address());
-                OFMatch ofMatch1 = new OFMatch();
-                ofMatch1.setNetworkDestination(0);
+                if (dst.getIPv4Address() == null) continue;
                 ofMatch.setNetworkDestination(dst.getIPv4Address());
                 SwitchCommands.removeRules(iofSwitch, table, ofMatch);
             }
